57aa5191f1782b3daddc20e1bb2ba0209a03f66a
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-02-20 10:13:18 -0600

    added a change to delete file.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,4 +1,4 @@
-DELETE name FROM albums WHERE release_date > 1991;
-DELETE name FROM albums WHERE genre = 'disco';
-DELETE name FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
 
