7ad80ff16a647feb43a681da95737eff5451c8ab
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-02-20 10:11:02 -0600

    Added more sql files.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,10 @@
 CREATE DATABASE IF NOT EXISTS codeup_test_db;
 USE codeup_test_db;
 CREATE TABLE IF NOT EXISTS albums (
-    id INT(10) unsigned NOT NULL AUTO_INCREMENT,
+    id INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
     artist VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
     release_date INT(4) NOT NULL,
     sales FLOAT(10,2) NOT NULL,
-    genre CHAR(100) NOT NULL,
-    primary key (id)
+    genre CHAR(100) NOT NULL
 );
---
+++ delete_exercises.sql
@@ -0,0 +1,4 @@
+DELETE name FROM albums WHERE release_date > 1991;
+DELETE name FROM albums WHERE genre = 'disco';
+DELETE name FROM albums WHERE artist = 'Whitney Houston';
+
